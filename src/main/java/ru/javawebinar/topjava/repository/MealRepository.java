22630c2881fa50331e3e580f74dff1a2eebe57ef
zaki <zak101@mail.ru>
2020-10-15 20:43:47 +0300

    3 02 HW2 repo filters

--- src/main/java/ru/javawebinar/topjava/repository/MealRepository.java
+++ src/main/java/ru/javawebinar/topjava/repository/MealRepository.java
@@ -2,6 +2,7 @@ package ru.javawebinar.topjava.repository;
 
 import ru.javawebinar.topjava.model.Meal;
 
+import java.time.LocalDateTime;
 import java.util.List;
 
 public interface MealRepository {
@@ -15,4 +16,7 @@ public interface MealRepository {
     Meal get(int id, int userId);
 
     List<Meal> getAll(int userId);
+
+    // ORDERED dateTime desc
+    List<Meal> getBetweenHalfOpen(LocalDateTime startDateTime, LocalDateTime endDateTime, int userId);
 }
