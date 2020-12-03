d43be5c28c94c74da93318e6450e6c75b6ff0f6c
Giorgiy <36568437+zaakki@users.noreply.github.com>
2020-12-03 20:25:17 +0300

    9 09 datatable via ajax

--- src/main/webapp/resources/js/topjava.users.js
+++ src/main/webapp/resources/js/topjava.users.js
@@ -21,6 +21,10 @@ $(function () {
     ctx = {
         ajaxUrl: userAjaxUrl,
         datatableApi: $("#datatable").DataTable({
+            "ajax": {
+                "url": userAjaxUrl,
+                "dataSrc": ""
+            },
             "paging": false,
             "info": true,
             "columns": [
@@ -28,24 +32,44 @@ $(function () {
                     "data": "name"
                 },
                 {
-                    "data": "email"
+                    "data": "email",
+                    "render": function (data, type, row) {
+                        if (type === "display") {
+                            return "<a href='mailto:" + data + "'>" + data + "</a>";
+                        }
+                        return data;
+                    }
                 },
                 {
                     "data": "roles"
                 },
                 {
-                    "data": "enabled"
+                    "data": "enabled",
+                    "render": function (data, type, row) {
+                        if (type === "display") {
+                            return "<input type='checkbox' " + (data ? "checked" : "") + " onclick='enable($(this)," + row.id + ");'/>";
+                        }
+                        return data;
+                    }
                 },
                 {
-                    "data": "registered"
+                    "data": "registered",
+                    "render": function (date, type, row) {
+                        if (type === "display") {
+                            return date.substring(0, 10);
+                        }
+                        return date;
+                    }
                 },
                 {
-                    "defaultContent": "Edit",
-                    "orderable": false
+                    "orderable": false,
+                    "defaultContent": "",
+                    "render": renderEditBtn
                 },
                 {
-                    "defaultContent": "Delete",
-                    "orderable": false
+                    "orderable": false,
+                    "defaultContent": "",
+                    "render": renderDeleteBtn
                 }
             ],
             "order": [
@@ -53,7 +77,12 @@ $(function () {
                     0,
                     "asc"
                 ]
-            ]
+            ],
+            "createdRow": function (row, data, dataIndex) {
+                if (!data.enabled) {
+                    $(row).attr("data-userEnabled", false);
+                }
+            }
         }),
         updateTable: function () {
             $.get(userAjaxUrl, updateTableByData);
