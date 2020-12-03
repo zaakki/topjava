5bb9c47283c0bfb92631d0eb6b111e03ea96f7cd
Giorgiy <36568437+zaakki@users.noreply.github.com>
2020-12-03 20:19:01 +0300

    9 07 update

--- src/main/webapp/resources/js/topjava.common.js
+++ src/main/webapp/resources/js/topjava.common.js
@@ -15,6 +15,16 @@ function add() {
     $("#editRow").modal();
 }
 
+function updateRow(id) {
+    form.find(":input").val("");
+    $.get(ctx.ajaxUrl + id, function (data) {
+        $.each(data, function (key, value) {
+            form.find("input[name='" + key + "']").val(value);
+        });
+        $('#editRow').modal();
+    });
+}
+
 function deleteRow(id) {
     if (confirm('Are you sure?')) {
         $.ajax({
